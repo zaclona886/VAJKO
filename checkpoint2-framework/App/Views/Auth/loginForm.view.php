dc61ac4b46349407495b3704d84b48f1c87a0697
Pavol Petrovič <palko.petrovic@gmail.com>
2021-12-01 11:25:48 +0100

    13

--- checkpoint2-framework/App/Views/Auth/loginForm.view.php
+++ checkpoint2-framework/App/Views/Auth/loginForm.view.php
@@ -1,6 +1,6 @@
 <?php /** @var Array $data */ ?>
 <div class="row">
-    <div class="col-0 col-md-6">
+    <div class="col-0 col-md-6 justify-content-center">
         <img class="card-img-top"
              src="https://i.pinimg.com/736x/f1/fc/01/f1fc012b7cf7f1d13cdfdcb8b66741d9.jpg" alt="...">
     </div>
